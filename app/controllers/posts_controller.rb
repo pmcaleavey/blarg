242fd60dfd41cbee269fcda5945937b98badd519
Pat McAleavey <pmcaleavey8@gmail.com>
2016-03-30 08:23:33 -0400

    working version - lots of comment to help me

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,55 @@
 class PostsController < ApplicationController
-  def index
-    posts = Post.all
-    render :index, locals: { posts: posts }
+  def index#get posts
+    @posts = Post.all##posts are all the blog posts#eventually use a gem to get a certain amount
+    render :index#, locals: { posts: posts }#what is passed in VIEWS ##call template, pass data to template --WHAT TEMPLATE??
+    ##render :index - (same as irb in sinatra) - rails creates this on its own if I don’t
   end
 
   def new
     render :new
+    #render looks to the 'view' foler for the 'post'(controller) folder, for the 'new.html.erb'
   end
 
   def create
     new_post = Post.create(title: params["title"],
                            content: params["content"])
+    redirect_to :root#after, take the user here
+    #^^directs to another method
+  end
+
+  def edit#display form
+    @post = Post.find(params["id"])#gets individual id from params hash
+    render :edit
+
+
+    #render :edit, locals: { post: post }#goes to edit view- creating locals variable named post = post
+    #edit- knows to look in the views folder, for posts folder (bec that's the controller name), edit.html.erb,
+    #post: post - 'post:' is what's passed in the views - 'post' is local variable in edit
+    #edit_post = Post.update(title: params["title"], content: params["content"])
+  end
+
+  def update
+    post = Post.find(params[:id])#have post user wants to update
+    post.update(title: params["title"], content: params["content"])
+    #calling Active Record 'update' method on post object(instance of post model)
+    #title: & content: match Post table - params["anyname"] -params['word']- is in edit _tag
+    redirect_to root_path
+
+    #create & update don't show/render, work w db
+    #update post based on params passed in
+  end
+
+  def destroy
+    post = Post.find(params["id"])
+    post.destroy
     redirect_to :root
+  end
+
+  def show
+    #post = Post.find(params["id"])  - can use this way
+    #render :show, locals: { post: post }
+
+    @post = Post.find(params["id"])
+    render :show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,56 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
 #  get "/", to: "posts#index"
-  root to: "posts#index"
-  get "posts/new", to: "posts#new", as: "posts_new"
-  post "posts", to: "posts#create"
+  root to: "posts#index" ##posts controller in index method
+  get "posts/new", to: "posts#new", as: "posts_new"##set up route that listens for a get request,
+                                                   ##if has that route, goes to posts_controller new method
+  ##^^url route specifies to go to posts_controller.rb and go to 'new' method in class
+
+
+  post "posts", to: "posts#create", as: "posts"
+
+  #Add a route to edit a specific post. It should have a route parameter.
+  get "posts/:id/edit/", to: "posts#edit", as: "edit_post"#displays # :id is route parameter
+  put "posts/:id", to: "posts#update", as: "post"#changes data in db
+  get "posts/:id", to: "posts#show" # as: post - not needed because same route as update
+  delete "posts/:id", to: "posts#destroy" # as: post
+
+
+  #create comment
+  # get "posts/:id/comments/new", to: "comments#new", as: "comments_new"
+  #
+  # <%= link_to("New comment", comments_new_path(post)) %>
+
+  #resources :posts -creates the 7, then use Rake Routes to see
+
+
+
+  #7 methods in controllers
+  #index, new/create, edit/update, show, destroy
+
+
+
+
+  #
+  # root to: "posts#index"
+  #   get "posts/new", to: "posts#new", as: "posts_new"
+  #   post "posts", to: "posts#create"
+  #
+  #   get "posts/:id/edit", to: "posts#edit", as: "edit_post"
+  #   put "posts/:id", to: "posts#update", as: "post"
+  #   get "posts/:id", to: "posts#show"
+  #   delete "posts/:id", to: "posts#destroy"
+
+
+
+
+
+
+
+
+
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
